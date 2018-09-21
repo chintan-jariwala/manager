08db326d811582776bf30e8942cae2efe42482af
chintanallstarz <chintanallstarz@gmail.com>
2018-09-20 19:25:16 -0700

    Added firebase with its config

--- src/App.js
+++ src/App.js
@@ -3,8 +3,15 @@ import {createStore} from 'redux';
 import {Provider} from 'react-redux';
 import {Container, Content, Header, Root, Title} from 'native-base';
 import reducers from './reducer';
+import * as config from './config/firebase-config'
+import firebase from 'firebase';
 
 class App extends Component {
+
+    componentWillMount() {
+        firebase.initializeApp(config);
+    }
+
     render() {
         return (
             <Root>
